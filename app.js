d8ce451f7c14bc0c4c983a54f5427f1d5242774f
Aldo Gatica <aldogatica123@gmail.com>
2018-10-01 23:58:20 -0600

    wishlist mock added

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let userRouter = require('./routes/user');
+let wishlistRouter = require('./routes/wishlist');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,28 +17,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
+app.use('/wishlist', wishlistRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  error = {
+    "message": err.message,
+    "status": err.status || 500
+  };
+  res.send(error)
 });
 
 module.exports = app;
 
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+    console.log('Wishlist app on http://localhost:5000!');
 });
 
